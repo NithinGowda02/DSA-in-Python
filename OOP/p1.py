5a42aa11f2118a43d1ad4c95feee0f171b592594
Nithin K P <nithinkp0212@gmail.com>
2025-10-09 22:01:09 +0530

    Added OOP Concept

---
+++ OOP/p1.py
@@ -0,0 +1,14 @@
+class car:
+    def __init__(self, name, model, price, milage):
+        self.name = name
+        self.model = model
+        self.price = price
+        self.milage = milage
+
+    def display(self):
+        print(f"{self.name} comes in Model {self.model} with best price Segment ${self.price} with milage of {self.milage}") 
+
+Toyata = car("Fortuner",2023,500000,8)
+Maruthi_Suzuki = car("Omni",2005,18000,11)
+Toyata.display()       
+Maruthi_Suzuki.display()    
